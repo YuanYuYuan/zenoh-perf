0017f6722002420609aa9967f6a4f3686665ef66
yuanyuyuan <az6980522@gmail.com>
2022-10-14 11:15:31 +0800

    Add native mosquitto execution

--- comparison/zenoh-kafka-mqtt/mqtt/scripts/run_mosquitto.sh
+++ comparison/zenoh-kafka-mqtt/mqtt/scripts/run_mosquitto.sh
@@ -3,22 +3,11 @@ set -e
 
 # Move to working dir
 script_dir="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
-cd "$script_dir/.."
-
-groups | grep -q docker || {
-    echo \"Warning: You may not have permission to execute docker. Try this command:\" >&2
-    echo \"         usermod -a -G docker YOUR_USERNAME\" >&2
-}
+cd "$script_dir/../mosquitto"
 
 VERSION=${1:-2.0.15}
 CONFIG=${2:-$script_dir/../mosquitto.conf}
 PORT=${3:-1883}
 CPUS=${4:-0,1}
 
-docker run \
-    --init \
-    --cpuset-cpus $CPUS \
-    -p $PORT:1883 \
-    --rm \
-    -v $CONFIG:/mosquitto/config/mosquitto.conf \
-    eclipse-mosquitto:$VERSION
+taskset -a -c $CPUS ./src/mosquitto -c $CONFIG -p $PORT
